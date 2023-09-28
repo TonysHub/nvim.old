b5316c7fee32af846a7d2c8d514fcc30572f4c66
Tony Lee <leegreen123@naver.com>
2023-09-29 00:55:04 +0900

    bufferline

--- after/plugin/bufferline.lua
+++ after/plugin/bufferline.lua
@@ -1,6 +1,19 @@
 local nnoremap = require("tonylee.keymap").nnoremap
 
 vim.opt.termguicolors = true
-require("bufferline").setup{}
+require("bufferline").setup {
+    options = {
+        offsets = {
+            {
+                filetype = "NvimTree",
+                text = function()
+                    return vim.fn.getcwd()
+                end,
+                highlight = "Directory",
+                text_align = "left"
+            }
+        }
+    }
+}
 nnoremap("<S-h>", "<cmd>BufferLineCyclePrev<cr>")
 nnoremap("<S-l>", "<cmd>BufferLineCycleNext<cr>")
